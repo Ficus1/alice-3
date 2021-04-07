fbd577073e74d86d57e2818899a5c30fb55ba35f
Ficus1 <74860665+Ficus1@users.noreply.github.com>
2021-04-07 19:57:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from flask import Flask, request
 import logging
 import json
+import os
 
 app = Flask(__name__)
 
@@ -68,4 +69,5 @@ def translate(word):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
